a2c40200d2414b824dd4587944ca080fafd24e46
monginadiana <dianahmongina2@gmail.com>
2022-06-24 11:46:42 +0300

    addition of the index.html file and js fiiles

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+document.querySelector('button').addEventListener('click', welcome)
+
+function welcome() {
+    let name = document.querySelector('input').value
+    document.querySelector('p').innerText = `Hello ${name} welcome to the bootcamp`
+}
